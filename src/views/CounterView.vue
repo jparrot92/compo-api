2a9c53706b76a9dec59fa97c9f736ddba26772ed
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-02 10:26:25 +0100

    useCounter composable

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -7,27 +7,17 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import useCounter from '@/composables/useCounter'
 
 export default {
     name: 'Counter',
     props: {},
     emits: [],
     setup() {
-        const counter = ref(5)
-
-        // const increase = () =>{
-        //     counter.value++
-        // }
-
-        // const decrease = () =>{
-        //     counter.value--
-        // }
+        const {counter, increase, decrease} = useCounter(25)
 
         return {
-            counter,
-            increase:() => counter.value--,
-            decrease:() => counter.value++,
+            counter, increase, decrease
         }
     },
 }
