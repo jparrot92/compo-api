d6f561b5784fba82def9157843c132d1709d7e18
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-02 09:20:11 +0100

    counter vue3

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,11 +1,34 @@
 <template>
-    <h1>Counter View</h1>    
+    <h1>Counter View</h1>
+    <h2>{{counter}}</h2>
+
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
+    name: 'Counter',
+    props: {},
+    emits: [],
     setup() {
-        
+        const counter = ref(5)
+
+        // const increase = () =>{
+        //     counter.value++
+        // }
+
+        // const decrease = () =>{
+        //     counter.value--
+        // }
+
+        return {
+            counter,
+            increase:() => counter.value--,
+            decrease:() => counter.value++,
+        }
     },
 }
 </script>
