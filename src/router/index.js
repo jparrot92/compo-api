a8245f431f7da41bbcf17694dfe097df0ef1d005
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-03 10:25:13 +0100

    Create users view

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,12 @@ const routes = [
     path: '/counter',
     name: 'counter',
     component: () => import(/* webpackChunkName: "counter" */ '../views/CounterView.vue')
-  }
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: () => import(/* webpackChunkName: "counter" */ '../views/UsersView.vue')
+  },
 ]
 
 const router = createRouter({
