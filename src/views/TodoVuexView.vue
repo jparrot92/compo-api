6f32dbb3c8efbbb83d00168b328af5526f7b280b
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-06 17:39:52 +0100

    Diseño de la lista de tareas

--- src/views/TodoVuexView.vue
+++ src/views/TodoVuexView.vue
@@ -4,8 +4,18 @@
     <h4>Pendientes: {{ pending.length }}</h4>
 
     <hr>
-    <h4>Todos: {{ all.length }}</h4>
-    <h4>Completados: {{ completed.length }}</h4>
+    <button class="active">Todos</button>
+    <button>Pendientes</button>
+    <button>Completados</button>
+
+    <div>
+        <ul>
+            <li v-for="todo in all" :key="todo.id"
+                :class="{ 'completed': todo.completed }">
+                {{ todo.text }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -25,6 +35,30 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+div {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+ul {
+    width: 300px;
+    text-align: left;
+}
+
+li {
+    cursor: pointer;
+}
+
+.active {
+    background-color: #2c3e50;
+    color: white;
+}
+
+.completed { 
+    text-decoration: line-through;
+}
 
 </style>
