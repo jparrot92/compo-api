9fa0d0bc3ed7702d2dc1ac220e2c10ea2a9b7b68
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-06 11:20:57 +0100

    Composable usePokemon

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,17 +1,36 @@
 <template>
-    <h1>Pokemon View</h1>
+    <h1 v-if="!pokemon && !errorMessage">Buscando...</h1>
+    <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
+
+    <template v-else>
+        <h3>{{ pokemon.name }}</h3>
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+        <br>
+        <router-link :to="{ name: 'pokemon-search'}">Regresar</router-link>
+    </template>
+
 </template>
 
 <script>
 import { useRoute } from 'vue-router'
+import usePokemon from '../composables/usePokemon'
 
 export default {
 
     setup() {
 
         const route = useRoute()
+        // console.log(route.params.id)
+        const { errorMessage, isLoading, pokemon, searchPokemon } = usePokemon( route.params.id )
 
-        console.log(route.params)
+        
+        return {
+            errorMessage,
+            isLoading,
+            pokemon
+        }
+
     }
+
 }
 </script>
